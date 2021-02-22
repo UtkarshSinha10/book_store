4ffaf619b35a6e399d15ee76efcb4492f59a13be
UtkarshSinha10 <utkarshsinha1010@gmail.com>
2021-02-22 12:59:49 +0530

    Linting of code

--- models/model_query/history_query.js
+++ models/model_query/history_query.js
@@ -1,33 +1,32 @@
-const { required } = require('joi');
-const mongoose = require('mongoose');
+/* eslint-disable max-len */
 const History = require('../model_schema/history');
-const Book = require('../model_schema/book');
-const User = require('../model_schema/user');
 
 exports.count_rented_books = async () =>{
-    try{
-        const total_rented_books = History.count({is_returned: false})
-    }catch(err){
-        throw err;
-    }
-}
+  try {
+    const total_rented_books = History.count({is_returned: false});
+    return total_rented_books;
+  } catch (err) {
+    throw err;
+  }
+};
 
-exports.find_book_by_book_id = async (book_name)=>{
-    try{
-        const rented_book_count = await History.count({book_id:book_id});
-        return rented_book_count;
-    }catch(err){
-        throw err;
-    }
-}
+exports.find_book_by_book_id = async ()=>{
+  try {
+    const rented_book_count = await History.count({book_id: book_id});
+    return rented_book_count;
+  } catch (err) {
+    throw err;
+  }
+};
 
 exports.find_rented_book_in_history = async (book_name) =>{
-    try{
-        const total_copies_rented = await History.count({book_name: book_name}).populate()
-    }catch(err){
-        throw err;
-    }
-}
+  try {
+    const total_copies_rented = await History.count({book_name: book_name}).populate();
+    return total_copies_rented;
+  } catch (err) {
+    throw err;
+  }
+};
 
 // exports.find_available_date = async(book_name) => {
 //     try{
--- models/model_query/user_query.js
+++ models/model_query/user_query.js
@@ -1,21 +1,20 @@
-const mongoose = require('mongoose');
 const User = require('../model_schema/user');
 
 exports.find_user = async (user) => {
-    try{
-        const found_user = await User.collection.findOne({email: user.email});
-        return found_user;
-    }catch(err){
-        throw err;
-    }
-}
+  try {
+    const found_user = await User.collection.findOne({email: user.email});
+    return found_user;
+  } catch (err) {
+    throw err;
+  }
+};
 
 
 exports.register_user = async (new_user) => {
-    try{
-        const register_user = await User.collection.insertOne(new_user);
-        return register_user;
-    }catch(err){
-        throw err;
-    }
-}
+  try {
+    const register_user = await User.collection.insertOne(new_user);
+    return register_user;
+  } catch (err) {
+    throw err;
+  }
+};
